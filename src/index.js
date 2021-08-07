13f93f8483bd322471c8569d70763cb366f33468
Anna Mugu <86118286+doublexkid@users.noreply.github.com>
2021-08-07 19:02:25 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function toReadable (number) {
+    const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+        'seventeen', 'eighteen', 'nineteen', 'twenty'];
+    const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty',
+        'ninety'];
+    
+    const numberString = number.toString();
+
+    if (number === 0) {
+        return 'zero';
+    }
+
+    if (number < 20) {
+        return ones[number];
+    }
+    else if (number >= 20 && number < 100) {
+        return (tens[Math.trunc(number / 10)] + (number % 10 > 0 ? ' ' + ones[number % 10].trim(): "")
+        );
+    }
+    else if (number >= 100) {
+        return (ones[Math.trunc(number / 100)] +
+            " hundred" +
+            (number % 100 == 0 ? "" : " " + toReadable(number % 100)));
+    }
+
+
+
+
+    toReadable(999);
+
   
 }
